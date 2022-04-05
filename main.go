059987b2e9065b92ff6f3a4dba78b83fa9c8db1b
atul <atulgutal007@gmail.com>
2022-04-05 10:05:25 -0700

    Implemented variables and constant

--- main.go
+++ main.go
@@ -3,5 +3,15 @@ package main
 import "fmt" 
 
 func main() {
-	fmt.Println("Hellow World")
+
+	var conferenceName = "Go Conference"
+	const conferenceTickets = 50
+	var remainingTickets = 50
+
+	fmt.Println("Welcome to", conferenceName, "booking application")
+	fmt.Println("We have total of", conferenceTickets, "tickets and", remainingTickets, "are stil available.")
+	fmt.Println("Get you tickets here to attend")
+
+	
+
 }
